be22ee2aca48436fcb6f4f97f81af1ab46d97e49
Duarte De Carvalho Machado Eusebio <dde-carv@c2r6s10.42lisboa.com>
2024-09-19 11:39:06 +0100

    changed .gitignore, corrected Makefile and changed header file.

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,7 +6,7 @@
 /*   By: dde-carv <dde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 14:00:02 by dde-carv          #+#    #+#             */
-/*   Updated: 2024/09/17 16:02:29 by dde-carv         ###   ########.fr       */
+/*   Updated: 2024/09/19 11:38:43 by dde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include "../libft/inc/libft.h"
 # include "../libft/inc/ft_printf.h"
 # include "../libft/inc/get_next_line.h"
-# include "../mlx.h"
 
 
 #endif
