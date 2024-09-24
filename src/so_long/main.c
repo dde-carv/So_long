e746b7d7186db2eee61e054255510e008476b386
Duarte De Carvalho Machado Eusebio <dde-carv@c2r6s9.42lisboa.com>
2024-09-24 12:43:29 +0100

    Corrected Makefile and image path.

--- src/so_long/main.c
+++ src/so_long/main.c
@@ -6,22 +6,11 @@
 /*   By: dde-carv <dde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:28:17 by dde-carv          #+#    #+#             */
-/*   Updated: 2024/09/24 11:47:15 by dde-carv         ###   ########.fr       */
+/*   Updated: 2024/09/24 12:41:31 by dde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/so_long.h"
-
-typedef struct	s_win
-{
-	void	*mlx;
-	void	*win;
-	void	*img;
-	int		img_width;
-	int		img_height;
-	int		x_pos;
-	int		y_pos;
-}				t_win;
 
 int	main(void)
 {
@@ -29,17 +18,14 @@ int	main(void)
 	char	*img_path;
 
 	test.x_pos = 300;
-	test.y_pos = 0;
-	img_path = "../../texture/male-deer.xpm";
+	test.y_pos = 300;
+	img_path = "./textures/male-deer.xpm";
 	test.mlx = mlx_init();
-	ft_printf("Ola\n");
 	test.win = mlx_new_window(test.mlx, 1000, 1000, "lol");
 	test.img = mlx_xpm_file_to_image(test.mlx, img_path, &test.img_width, &test.img_height);
-	ft_printf("Ola1\n");
+	mlx_put_image_to_window(test.mlx, test.win, test.img, test.x_pos, test.y_pos);
 	mlx_hook(test.win, DestroyNotify, NoEventMask, ft_close, &test);
-	ft_printf("Ola2\n");
 	mlx_hook(test.win, KeyPress, KeyPressMask, ft_keycode, &test);
-	ft_printf("Ola3\n");
 	mlx_loop(test.mlx);
 	return (0);
 }
--- src/so_long/so_long_utils.c
+++ src/so_long/so_long_utils.c
@@ -6,7 +6,7 @@
 /*   By: dde-carv <dde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 11:18:04 by dde-carv          #+#    #+#             */
-/*   Updated: 2024/09/24 11:39:30 by dde-carv         ###   ########.fr       */
+/*   Updated: 2024/09/24 12:41:54 by dde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,26 +29,21 @@ int	ft_keycode(int keycode, t_win *test)
 	}
 	if (keycode == XK_a)
 	{
-		ft_printf("Ola4\n");
 		mlx_clear_window(test->mlx, test->win);
 		mlx_put_image_to_window(test->mlx, test->win, test->img, test->x_pos = test->x_pos - test->img_width, test->y_pos);
 	}
 	if (keycode == XK_d)
 	{
-		ft_printf("Ola5\n");
 		mlx_clear_window(test->mlx, test->win);
 		mlx_put_image_to_window(test->mlx, test->win, test->img, test->x_pos = test->x_pos + test->img_width, test->y_pos);
 	}
 	if (keycode == XK_w)
 	{
-		ft_printf("Ola6\n");
 		mlx_clear_window(test->mlx, test->win);
-		ft_printf("Ola8\n");
 		mlx_put_image_to_window(test->mlx, test->win, test->img, test->x_pos, test->y_pos = test->y_pos - test->img_height);
 	}
 	if (keycode == XK_s)
 	{
-		ft_printf("Ola7\n");
 		mlx_clear_window(test->mlx, test->win);
 		mlx_put_image_to_window(test->mlx, test->win, test->img, test->x_pos, test->y_pos = test->y_pos + test->img_height);
 	}
